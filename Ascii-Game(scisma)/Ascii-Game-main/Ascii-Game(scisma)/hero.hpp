9c6343656fd70fd18f2ec9868d1d63fd4bf1a84f
Jean-cube <72654048+Jean-cube@users.noreply.github.com>
2021-10-09 11:24:29 +0200

    Add files via upload

---
+++ Ascii-Game(scisma)/Ascii-Game-main/Ascii-Game(scisma)/hero.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "Cast.hpp"
+
+class hero : public cast{
+protected: 
+    int life;
+    int bullet;
+    bool delay;
+
+public:
+
+    hero();
+    void decreaseLife();
+    void setLife(int newLife);
+    void setBullet(int newBullet);
+    void decreaseBullet();
+    int getLife();
+    int getBullet();
+    void changeDelay();
+    void setDelay(bool newDelay);
+    bool getDelay();
+};
